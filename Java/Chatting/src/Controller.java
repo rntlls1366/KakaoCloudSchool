83188cf5137b89a9a63742c7d6efa8b126e33942
KJY-kpu <rntlls1366@gmail.com>
2023-06-23 18:11:14 +0900

    수정

--- Java/Chatting/src/Controller.java
+++ Java/Chatting/src/Controller.java
@@ -26,60 +26,56 @@ class Controller extends Thread {
 
     public synchronized void run() {
         while (true) {
-            //synchronized (this) {
-            try {
-                sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            if (Messages.size() > 0) {
-                System.out.println("Message Queue is Exist!");
-                Message message = Messages.poll();
-                if (message.content.equals("help")) {
-                    System.out.println(sockMap.size());
-                    System.out.println(Messages.size());
-                }
-                if (message.recieverId.equals("ALL")) {
-                    Set<String> keySet = sockMap.keySet();
-                    for (String key : keySet) {
+            synchronized (this) {
 
-                        if (!key.equals(message.senderId)) {
+                if (Messages.size() > 0) {
+                    System.out.println("Message Queue is Exist!");
+                    Message message = Messages.poll();
+                    if (message.content.equals("help")) {
+                        System.out.println(sockMap.size());
+                        System.out.println(Messages.size());
+                    }
+                    if (message.recieverId.equals("ALL")) {
+                        Set<String> keySet = sockMap.keySet();
+                        for (String key : keySet) {
 
-                            byte[] buffer;
-                            try {
-                                buffer = message.content.getBytes("UTF-8");
-                            } catch (UnsupportedEncodingException ex) {
-                                throw new RuntimeException(ex);
-                            }
-                            try {
-                                System.out.println("Send Message to " + key + " Output is " + sockMap.get(key).toString());
-                                System.out.println("Buffer.length is " + buffer.length);
-                                System.out.write(buffer);
-                                sockMap.get(key).write(buffer, 0, buffer.length);
-                                //sockMap.get(key).write(buffer, 0, buffer.length);
+                            if (!key.equals(message.senderId)) {
 
-                            } catch (IOException ex) {
-                                throw new RuntimeException(ex);
+                                byte[] buffer;
+                                try {
+                                    buffer = message.content.getBytes("UTF-8");
+                                } catch (UnsupportedEncodingException ex) {
+                                    throw new RuntimeException(ex);
+                                }
+                                try {
+                                    System.out.println("Send Message to " + key + " Output is " + sockMap.get(key).toString());
+                                    System.out.println("Buffer.length is " + buffer.length);
+                                    System.out.write(buffer);
+                                    sockMap.get(key).write(buffer, 0, buffer.length);
+                                    sockMap.get(key).close();
+
+                                } catch (IOException ex) {
+                                    throw new RuntimeException(ex);
+                                }
                             }
                         }
-                    }
-                } else {
-                    byte[] buffer;
-                    try {
-                        buffer = message.content.getBytes("UTF-8");
-                    } catch (UnsupportedEncodingException ex) {
-                        throw new RuntimeException(ex);
-                    }
-                    try {
-                        System.out.println("Send Message One : Out check :  " + sockMap.get(message.recieverId).toString());
-                        System.out.write(buffer);
-                        sockMap.get(message.recieverId).write(buffer, 0, 1024);
-                    } catch (IOException ex) {
-                        throw new RuntimeException(ex);
+                    } else {
+                        byte[] buffer;
+                        try {
+                            buffer = message.content.getBytes("UTF-8");
+                        } catch (UnsupportedEncodingException ex) {
+                            throw new RuntimeException(ex);
+                        }
+                        try {
+                            System.out.println("Send Message One : Out check :  " + sockMap.get(message.recieverId).toString());
+                            System.out.write(buffer);
+                            sockMap.get(message.recieverId).write(buffer, 0, 1024);
+                        } catch (IOException ex) {
+                            throw new RuntimeException(ex);
+                        }
                     }
                 }
             }
-            //}
         }
     }
 }
